ba0c5a918012b9ea19f6b339cb42d37af0c3514c
Besong Wisdom <engineerbesong@gmail.com>
2026-05-09 04:38:38 +0100

    Fix build configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['10.60.190.146', 'localhost'],
+  images: {
+    domains: ['res.cloudinary.com', 'placehold.co'],
+  },
 };
 
 export default nextConfig;
